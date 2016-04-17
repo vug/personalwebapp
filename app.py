36ef6b5eac46d1a14070bcae8882d45f1d8c62d5
Ugur Guney <ugurguney@gmail.com>
2016-04-17 15:10:36 -0400

    fixed spaces

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hi, I am Ugur!"
+    return "Hi, I am Ugur! This is my personal web app."
+
 
 if __name__ == "__main__":
     app.run()
-
