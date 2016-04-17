6de5f669f2646d7ca885ec32bb0004d787fb7eb0
Ugur Guney <ugurguney@gmail.com>
2016-04-17 15:49:49 -0400

    turned debug on. render_template home.html

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hi, I am Ugur! This is my personal web app."
+def index():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
