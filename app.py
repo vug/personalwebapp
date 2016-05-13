ce8ff2864ddb66017d625ff45c472717daf1a898
Ugur Guney <ugurguney@gmail.com>
2016-05-13 01:23:47 -0400

    random colors in the background

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sys
+import random
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def rnd_clr():
+	colors = ['#9ad3de', 'rgb(252,123,52)', '#3fb0ac', '#fae596', '#dbe9d8', '#f2efe8', '#fccdd3']
+	return random.choice(colors)
+
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('home.html', bg_color=rnd_clr())
 
 
 if __name__ == "__main__":
