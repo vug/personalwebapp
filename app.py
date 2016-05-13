358c7dca1f26e5c39d322fd18209383e3262a938
Ugur Guney <ugurguney@gmail.com>
2016-05-13 01:02:14 -0400

    choose port as a command line argument. (default: 8000)

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +11,7 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	port = 8000
+	if len(sys.argv) >= 2:
+		port = int(sys.argv[1])
+	app.run(port=port, debug=True)
