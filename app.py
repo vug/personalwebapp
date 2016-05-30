e3321f01a7cf2952bca24cd4881ac1dafcf7bb06
Ugur Guney <ugurguney@gmail.com>
2016-05-30 15:52:49 -0400

    code reformat

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ Misaka(app)
 
 
 def rnd_clr():
-	colors = ['#9ad3de', 'rgb(252,123,52)', '#3fb0ac', '#fae596', '#dbe9d8', '#f2efe8', '#fccdd3']
-	return random.choice(colors)
+    colors = ['#9ad3de', 'rgb(252,123,52)', '#3fb0ac', '#fae596', '#dbe9d8', '#f2efe8', '#fccdd3']
+    return random.choice(colors)
 
 
 @app.route('/<name>')
 def static_page(name):
-	if name in static_pages:
-		return render_template(name, bg_color=rnd_clr())
-	else:
-		return redirect(url_for('index'))
+    if name in static_pages:
+        return render_template(name, bg_color=rnd_clr())
+    else:
+        return redirect(url_for('index'))
 
 
 @app.route('/')
@@ -46,7 +46,7 @@ def blog_post(post):
 
 
 if __name__ == "__main__":
-	port = 8000
-	if len(sys.argv) >= 2:
-		port = int(sys.argv[1])
-	app.run(port=port, debug=True)
+    port = 8000
+    if len(sys.argv) >= 2:
+        port = int(sys.argv[1])
+    app.run(port=port, debug=True)
