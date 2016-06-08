85d2e2e18dc8f65a2687073a0ebfb6c588f0439f
Ugur Guney <ugurguney@gmail.com>
2016-06-07 23:16:55 -0400

    get meta data from post markdown files

--- app.py
+++ app.py
@@ -39,10 +39,13 @@ def blog():
 def blog_post(post):
     with open(os.path.join('posts', post), 'rt') as f:
         md_text = f.read()
-        html = markdown(md_text, fenced_code=True, math=True)
+    html = markdown(md_text, fenced_code=True, math=True)
+    first_three_lines = md_text.split('\n')[:3]
+    title, date, tags = [line.split('(')[1][:-1] for line in first_three_lines]
+    tags = tags.split(',')
     # https://flask-misaka.readthedocs.io/en/latest/
     # http://misaka.61924.nl/#
-    return render_template('blog.html', post=html)
+    return render_template('blog_post.html', post=html, date=date, tags=tags)
 
 
 if __name__ == "__main__":
