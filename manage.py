ce72ed6c7824789dadc78bbf6c83298abaf905fa
Ugur Guney <ugurguney@gmail.com>
2016-11-12 22:07:14 -0500

    manager command to create a Personalwebapp user

--- manage.py
+++ manage.py
@@ -10,5 +10,15 @@ def create_db():
     db.create_all()
 
 
+@manager.option('--email', help='email of the user')
+@manager.option('--password', help='password of the user')
+@manager.option('--fullname', help='fullname of the user')
+def create_user(email, password, fullname):
+    user = app.User(email, password, fullname)
+    db.session.add(user)
+    db.session.commit()
+    print('New user with email address "{}" has been generated'.format(email))
+
+
 if __name__ == '__main__':
     manager.run()
