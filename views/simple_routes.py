f324bb0767c1720492050d6fefe1504b4b4a35e6
Ugur Guney <ugurguney@gmail.com>
2016-11-13 10:13:57 -0500

    template for admin page

--- views/simple_routes.py
+++ views/simple_routes.py
@@ -1,7 +1,7 @@
 """
 This Blueprint collects simple views that does not have their own Blueprint yet.
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required
 
 simple_routes = Blueprint('simple_routes', __name__)
@@ -10,4 +10,4 @@ simple_routes = Blueprint('simple_routes', __name__)
 @simple_routes.route('/admin')
 @login_required
 def admin():
-    return 'Welcome to admin page. You must be a PersonalWebApp user.'
+    return render_template('admin.html')
