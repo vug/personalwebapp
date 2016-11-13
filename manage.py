f525412c2803d26714231e8ff802cd122399a05a
Ugur Guney <ugurguney@gmail.com>
2016-11-12 22:06:48 -0500

    manager command to create database

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 from flask_script import Manager
 
-from app import app
+from app import app, db
 
 manager = Manager(app)
+
+
+@manager.command
+def create_db():
+    db.create_all()
 
 
 if __name__ == '__main__':
