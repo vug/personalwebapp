92b76950c86b8da8c4bdc5ab44bdaad4c845e771
Ugur Guney <ugurguney@gmail.com>
2016-11-13 09:32:20 -0500

    add test for blog. rename test cases.

--- tests.py
+++ tests.py
@@ -17,7 +17,7 @@ class MyTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_index(self):
+    def test_can_access_static_pages(self):
         rv = self.app.get('/')
         assert b'home_page' in rv.data
 
@@ -30,8 +30,13 @@ class MyTest(unittest.TestCase):
         rv = self.app.get('/projects.html')
         assert b'projects_page' in rv.data
 
+    def test_can_access_login(self):
         rv = self.app.get('/login')
         assert b'login_page' in rv.data
+        
+    def test_can_access_blog(self):
+        rv = self.app.get('/blog/')
+        assert b'blog_posts_list_page' in rv.data
 
 
 if __name__ == '__main__':
