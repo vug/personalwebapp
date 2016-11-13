3bf6cee604ea2c08b86878c4eb5044512e5e9614
Ugur Guney <ugurguney@gmail.com>
2016-11-13 00:58:19 -0500

    manager command to generate secret.py

--- manage.py
+++ manage.py
@@ -23,5 +23,16 @@ def create_user(email, password, fullname):
     print('New user with email address "{}" has been generated'.format(email))
 
 
+@manager.command
+def gen_secret():
+    """Generate a secret_key for Flask-Login security and write it into secret.py."""
+    import os
+    import binascii
+
+    key = binascii.hexlify(os.urandom(24))
+    with open('secret.py', 'wt') as f:
+        f.write('SECRET_KEY = {}'.format(key))
+
+
 if __name__ == '__main__':
     manager.run()
