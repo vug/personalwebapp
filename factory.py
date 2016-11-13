fb436db764f1cef775437f878c80f58fa99de58a
Ugur Guney <ugurguney@gmail.com>
2016-11-13 08:33:00 -0500

    argument for create_app to override existing configuration

--- factory.py
+++ factory.py
@@ -7,11 +7,14 @@ from extensions import login_manager, misaka, db
 from views import static_pages, blog, login, simple_routes
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # sqlite://<nohostname>/<path>
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_pyfile('secret.py', silent=True)
+    if config is not None:
+        app.config.update(config)
 
     misaka.init_app(app)
     login_manager.init_app(app)
