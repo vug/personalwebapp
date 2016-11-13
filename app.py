1b66669f0a60c23f5fc32462b759455bdcf7e104
Ugur Guney <ugurguney@gmail.com>
2016-11-13 00:39:53 -0500

    create_app function in factory module

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-from flask import Flask
 from flask_login import login_required
 
-from extensions import login_manager, misaka, db
-from views import static_pages, blog, login
+from factory import create_app
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # sqlite://<nohostname>/<path>
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config.from_pyfile('secret.py', silent=True)
-
-misaka.init_app(app)
-login_manager.init_app(app)
-db.init_app(app)
-
-app.register_blueprint(login)
-app.register_blueprint(static_pages)
-app.register_blueprint(blog, url_prefix='/blog')
+app = create_app()
 
 
 @app.route('/admin')
