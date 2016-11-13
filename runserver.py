1428dfefd15301b53bd451a61964eb7d3ba6eb7c
Ugur Guney <ugurguney@gmail.com>
2016-11-13 01:00:57 -0500

    use argparser in runserver.py

--- runserver.py
+++ runserver.py
@@ -1,9 +1,11 @@
-import sys
+import argparse
+
 from factory import create_app
 
 if __name__ == "__main__":
-    port = 8000
-    if len(sys.argv) >= 2:
-        port = int(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, help='port to listen to', default=8000)
+    args = parser.parse_args()
+
     app = create_app()
-    app.run(port=port, debug=True)
+    app.run(port=args.port, debug=True)
