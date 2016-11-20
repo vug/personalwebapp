8fc43bc470d550ef03981b22cb4600180346b098
Ugur Guney <ugurguney@gmail.com>
2016-11-20 00:19:50 -0500

    /log_report route the calls a shell command which generates an nginx log analysis dashboard

--- views/simple_routes.py
+++ views/simple_routes.py
@@ -1,7 +1,9 @@
 """
 This Blueprint collects simple views that does not have their own Blueprint yet.
 """
-from flask import Blueprint, render_template
+import subprocess
+
+from flask import Blueprint, render_template, send_file
 from flask_login import login_required
 
 simple_routes = Blueprint('simple_routes', __name__)
@@ -11,3 +13,14 @@ simple_routes = Blueprint('simple_routes', __name__)
 @login_required
 def admin():
     return render_template('admin.html')
+
+
+@simple_routes.route('/log_report')
+@login_required
+def log_report():
+    """Generate a nginx log report dashboard using Goaccess.
+
+    This is meant to be run on the EC2 instance with goaccess installed in the system PATH."""
+    cmd = 'goaccess -f /home/ubuntu/personalwebapp/logs/nginx/access.log -a -o /home/ubuntu/personalwebapp/instance/report.html'
+    subprocess.call(cmd, shell=True)
+    return send_file('instance/report.html')
